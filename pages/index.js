85d1f628bc7e1105c22fed922d2b659cc6254b62
EndePointe <alvin.johnsii@gmail.com>
2020-07-20 22:42:38 -0700

    ssdd

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,31 @@ import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
 //
 import Head from 'next/head'
 
+import {
+  useState,
+} from 'react';
 
 
 export default function Home() {
+
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+    console.log('toggle menu')
+    let el = document.querySelector('.HiddenMenu');
+    console.log(el);
+  }
+
   return (
     <div>
       <Head>
         <title>Ecomm_1</title>
       </Head>
       <nav className={navStyles.Nav}>
-        <button className={navStyles.MenuBtn}>
+        <button 
+          onClick={toggleMenu}
+          className={navStyles.MenuBtn}>
           <MenuIcon
             className={navStyles.MenuIcon}
             fontSize='large'
@@ -44,6 +59,12 @@ export default function Home() {
           </li>
         </ul>
       </nav>
+
+      <aside 
+        className={navStyles.HiddenMenu}>
+        hidden content
+      </aside>
+
       <header className={headerStyles.Header}>
         <div className={headerStyles.H1s}>
           <h1 className={headerStyles.H1}>Time to start selling.</h1>
@@ -51,7 +72,7 @@ export default function Home() {
         </div>
         <div className={headerStyles.ShopNowDiv}>
           <button className={headerStyles.ShopNowBtn}>
-            Show Now
+            Shop Now
             </button>
         </div>
       </header>
