7ca860d705777ac33e7f2c5e3d33f02475f9edb7
EndePointe <endepointe@gmail.com>
2020-07-27 08:08:43 +0100

    pgp issues

--- db/pgp/pgp.js
+++ db/pgp/pgp.js
@@ -1,16 +1,16 @@
 const initOptions = {
-  // connect(client, dc, useCount) {
-  //   const cp = client.connectionParameters;
-  //   // console.log('Connected to db: ' + cp.database);
-  //   // console.log('CP: ' + cp);
-  // },
-  // disconnect(client, dc) {
-  //   const cp = client.connectionParameters;
-  //   // console.log('Disconnecting from db: ' + cp.database);
-  // }
+  connect(client, dc, useCount) {
+    const cp = client.connectionParameters;
+    console.log('Connected to db: ' + cp.database);
+    console.log('CP: ' + cp);
+  },
+  disconnect(client, dc) {
+    const cp = client.connectionParameters;
+    console.log('Disconnecting from db: ' + cp.database);
+  }
 }
 
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')(initOptions);
 
 const cn = {
   host: 'localhost',
@@ -18,7 +18,7 @@ const cn = {
   database: 'clerk',
   user: process.env.PSQLUSER,
   password: process.env.PSQLPASS,
-  max: 2
+  max: 1
 };
 
 const db = pgp(cn);
